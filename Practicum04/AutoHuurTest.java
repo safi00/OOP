18d192ecb75de895b36124f27b3307b8d4a9ef72
Emma971 <57229866+Emma971@users.noreply.github.com>
2020-03-09 11:48:46 +0100

    9/3/2020 11:48

--- Practicum04/AutoHuurTest.java
+++ Practicum04/AutoHuurTest.java
@@ -9,30 +9,51 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AutoHuurTest {
-    private KlantB h;
     private AutoHuurB aH1;
 
     @BeforeEach
     public void init() {
         System.out.println("init");
         aH1 = new AutoHuurB();
-        h = new KlantB("Mijnheer de Vries");
+
+        AutoB a = new AutoB("Ferrari", 5000.00);
+        KlantB b = new KlantB("Mijnheer de Vries");
+
+        aH1.setGehuurdeAuto(a);
+        aH1.setHuurder(b);
     }
 
     @Test
     void geenHuurderGeenAuto() {
         System.out.println("geenHuurderGeenAuto");
-        assertEquals(null, aH1.getGehuurdeAuto());
-        assertEquals(null, aH1.getHuurder());
+        aH1.setHuurder(null);
+        aH1.setGehuurdeAuto(null);
+
+        assertEquals(("er is geen auto bekend\n" + "er is geen huurder bekend\n"+ "aantal dagen: 1 en dat kost 0.0"), aH1.toString());
+
     }
 
     @Test
-    void welHuurderGeenAuto() {
-        System.out.println("welHuurderGeenAuto");
-        KlantB k = new KlantB("Mijnheer de Vries");
+    void geenHuurderWelAuto(){
+        System.out.println("geenHuurderWelAuto");
+        aH1.setHuurder(null);
+
+        assertEquals("autotype: Ferrari met prijs per dag: 5000.0\n" + "er is geen huurder bekend\n"+ "aantal dagen: 1 en dat kost 0.0", aH1.toString());
+    }
+
+    @Test
+    void welHuuderWelAuto(){
+        System.out.println("welHuurderWelAuto");
+
+        assertEquals("autotype: Ferrari met prijs per dag: 5000.0\n" + "op naam van: Mijnheer de Vries (korting: 0.0%)\n"+ "aantal dagen: 1 en dat kost 5000.0", aH1.toString());
+    }
+
+    @Test
+    void huurderMetKorting() {
+        System.out.println("huurderMetKorting");
+        KlantB k = new KlantB("Jan");
         aH1.setHuurder(k);
-        assertEquals(null, aH1.getGehuurdeAuto());
-      //  assertEquals("op naam van: Mijnheer de Vries (korting: 0.0%)", aH1.getHuurder());
-        assertEquals(h.toString(), aH1.getHuurder());
+        k.setKorting(10);
+        assertEquals("autotype: Ferrari met prijs per dag: 5000.0\n" + "op naam van: Jan (korting: 10.0%)\n"+ "aantal dagen: 1 en dat kost 4500.0", aH1.toString());
     }
 }
